2e1b70fa3689662e1552135376d7b871edce76ac
Alex Saunders <alexsaunderscodes@gmail.com>
2021-09-07 13:07:30 -0400

    added dropdown subject id selection

--- docs/plots.js
+++ docs/plots.js
@@ -1,16 +1,15 @@
-
-function init() {
-    target = top_ten("940")[0];
+function create_bars() {
+    id_target = d3.select("#subjectSelect").property("value");
+    target = data.samples.filter(sample => sample.id === id_target)[0];
     let trace = {
-        x: target.sample_values.slice(0,10),
-        y: target.otu_ids.slice(0,10),
-        hovertext: target.otu_labels.slice(0,10),
+        x: target.sample_values.slice(0,10).reverse(),
+        y: target.otu_ids.slice(0,10).reverse(),
+        hovertext: target.otu_labels.slice(0,10).reverse(),
         type: "bar",
         orientation: 'h'
     };
-
     let layout = {
-        title: "Top Ten OTU's In Subject 940",
+        title: `Top Ten OTU's In Subject ${id_target}`,
         yaxis: {
             type: 'category',
 
@@ -20,11 +19,11 @@ function init() {
     Plotly.newPlot("plot1", traceData, layout);
 }
 
-init();
+const idBar = document.getElementById("subjectSelect");
 
-function top_ten(id_target) {
-    let target_patient = data.samples.filter(sample => sample.id === id_target)
-    return target_patient
+for (i=0; i< data.samples.length; i++) {
+    idBar.innerHTML += `<option value =  "${data.samples[i].id}">${data.samples[i].id}</option>`
+};
+d3.select('#subjectSelect').on('change', create_bars)
 
-}   
-
+create_bars();
