94793fa2fc0522d97fb2c5db642eec448cbd3d11
Alex Saunders <alexsaunderscodes@gmail.com>
2021-09-09 10:33:32 -0400

    made demographics

--- docs/plots.js
+++ docs/plots.js
@@ -1,6 +1,4 @@
-function create_bars() {
-    id_target = d3.select("#subjectSelect").property("value");
-    target = data.samples.filter(sample => sample.id === id_target)[0];
+function create_bars(target) {
     let trace = {
         x: target.sample_values.slice(0,10).reverse(),
         y: target.otu_ids.slice(0,10).reverse(),
@@ -9,14 +7,59 @@ function create_bars() {
         orientation: 'h'
     };
     let layout = {
-        title: `Top Ten OTU's In Subject ${id_target}`,
+        title: `Top Microbial Taxonomic Units In Subject ${target.id}`,
         yaxis: {
-            type: 'category',
-
-        }
+            type: 'category'
+        },
+        height: 600,
+        width: 1000
     }
     traceData= [trace];
-    Plotly.newPlot("plot1", traceData, layout);
+    Plotly.newPlot("barChart", traceData, layout);
+};
+
+function create_bubbles(target) {
+    let trace = {
+        x: target.otu_ids.slice(0,10).reverse(),
+        y: target.sample_values.slice(0,10).reverse(),
+        mode: "markers",
+        marker: {
+            color: target.otu_ids.slice(0,10).reverse(),
+            size: target.sample_values.slice(0,10).reverse(),
+        },
+        text: target.otu_labels.slice(0,10).reverse(),
+    };
+    let layout = {
+        title: `Top Microbial Taxonomic Units In Subject ${target.id}`,
+        yaxis: {
+            type: 'category'
+        },
+        height: 600,
+        width: 1000
+    };
+    traceData= [trace];
+    Plotly.newPlot("bubbleChart", traceData, layout);
+};
+
+function populate_demographics(id_target) {
+    console.log(id_target);
+    metadata = data.metadata.filter(subject => subject.id === parseInt(id_target))[0];
+    console.log(metadata)
+    document.getElementById("id").innerHTML = metadata.id;
+    document.getElementById("ethnicity").innerHTML = metadata.ethnicity;
+    document.getElementById("gender").innerHTML = metadata.gender;
+    document.getElementById("age").innerHTML = metadata.age;
+    document.getElementById("location").innerHTML = metadata.location;
+    document.getElementById("bbtype").innerHTML = metadata.bbtype;
+    document.getElementById("wfreq").innerHTML = metadata.wfreq;
+}
+
+function make_plots() {
+    id_target = d3.select("#subjectSelect").property("value");
+    target = data.samples.filter(sample => sample.id === id_target)[0];
+    create_bars(target);
+    create_bubbles(target);
+    populate_demographics(id_target);
 }
 
 const idBar = document.getElementById("subjectSelect");
@@ -24,6 +67,6 @@ const idBar = document.getElementById("subjectSelect");
 for (i=0; i< data.samples.length; i++) {
     idBar.innerHTML += `<option value =  "${data.samples[i].id}">${data.samples[i].id}</option>`
 };
-d3.select('#subjectSelect').on('change', create_bars)
+d3.select('#subjectSelect').on('change', make_plots)
 
-create_bars();
+make_plots();
