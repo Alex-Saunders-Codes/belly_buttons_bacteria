1e48f0f7e7e7c2190f41c06b945c708f4f467074
Alex Saunders <alexsaunderscodes@gmail.com>
2021-09-09 15:07:35 -0400

    Finished website

--- docs/plots.js
+++ docs/plots.js
@@ -11,8 +11,8 @@ function create_bars(target) {
         yaxis: {
             type: 'category'
         },
-        height: 600,
-        width: 1000
+        height: 400,
+        width: 600
     }
     traceData= [trace];
     Plotly.newPlot("barChart", traceData, layout);
@@ -34,17 +34,16 @@ function create_bubbles(target) {
         yaxis: {
             type: 'category'
         },
-        height: 600,
-        width: 1000
+        height: 400,
+        width: 600
     };
     traceData= [trace];
     Plotly.newPlot("bubbleChart", traceData, layout);
 };
 
 function populate_demographics(id_target) {
-    console.log(id_target);
     metadata = data.metadata.filter(subject => subject.id === parseInt(id_target))[0];
-    console.log(metadata)
+    document.getElementById("demographics").style.visibility = "visible";
     document.getElementById("id").innerHTML = metadata.id;
     document.getElementById("ethnicity").innerHTML = metadata.ethnicity;
     document.getElementById("gender").innerHTML = metadata.gender;
@@ -54,19 +53,59 @@ function populate_demographics(id_target) {
     document.getElementById("wfreq").innerHTML = metadata.wfreq;
 }
 
+function create_gauge(id_target) {
+    metadata = data.metadata.filter(subject => subject.id === parseInt(id_target))[0];
+    wfreq = metadata.wfreq;
+    let trace = [
+        {
+          type: "indicator",
+          mode: "gauge",
+          value: wfreq,
+          title: { text: "Belly Button Washing Frequency", font: { size: 24 }},
+          gauge: {
+            axis: { range: [0, 9], dtick: 1, tickcolor: "black"},
+            bar: { color: "black", thickness:0.3 },
+            bgcolor: "white",
+            borderwidth: 1,
+            bordercolor: "black",
+            steps: [
+              { range: [0, 1], color: "#CD5C5C", line: {color:"black", width:1} },
+              { range: [1, 2], color: "#C6595E", line: {color:"black", width:1} },
+              { range: [2, 3], color: "#BF5661", line: {color:"black", width:1} },
+              { range: [3, 4], color: "#B85363", line: {color:"black", width:1} },
+              { range: [4, 5], color: "#B15065", line: {color:"black", width:1} },
+              { range: [5, 6], color: "#AA4D67", line: {color:"black", width:1} },
+              { range: [6, 7], color: "#A34A6A", line: {color:"black", width:1} },
+              { range: [7, 8], color: "#9C476C", line: {color:"black", width:1} },
+              { range: [8, 9], color: "#95446E", line: {color:"black", width:1} }
+            ],
+          }
+        }
+      ];
+      
+      var layout = {
+        width: 500,
+        height: 400,
+        margin: { t: 25, r: 25, l: 25, b: 25 },
+        font: { color: "black", family: "Arial" }
+      };
+      
+      Plotly.newPlot('gaugeChart', trace, layout);
+};
+
 function make_plots() {
+    document.getElementById("greeting").style.display="none";
     id_target = d3.select("#subjectSelect").property("value");
     target = data.samples.filter(sample => sample.id === id_target)[0];
     create_bars(target);
     create_bubbles(target);
     populate_demographics(id_target);
+    create_gauge(id_target)
 }
 
 const idBar = document.getElementById("subjectSelect");
 
 for (i=0; i< data.samples.length; i++) {
-    idBar.innerHTML += `<option value =  "${data.samples[i].id}">${data.samples[i].id}</option>`
+    idBar.innerHTML += `<option value =  "${data.samples[i].id}" class="dropdown-item">${data.samples[i].id}</option>`
 };
 d3.select('#subjectSelect').on('change', make_plots)
-
-make_plots();
